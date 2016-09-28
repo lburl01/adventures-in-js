5645eb5783b6c8013cb7f65ee54d65a47fdaed56
Lori Burleson <lori.burleson@gmail.com>
2016-09-28 17:28:21 -0400

    Full sequence of slides built and will transition successfully.

--- _site/adventures.js
+++ _site/adventures.js
@@ -1,5 +1,16 @@
 
 $( ".btn" ).click(function() {
   $( "#slide-one" ).hide( "slow")
+  // body.removeClass().addClass("slide-two")
   $( "#slide-two" ).show( "slow")
+});
+
+$( ".mtns" ).click(function() {
+  $( "#slide-two" ).hide( "slow")
+  $( "#slide-three-mtn" ).show( "slow")
+});
+
+$( ".river" ).click(function() {
+  $( "#slide-two" ).hide( "slow")
+  $( "#slide-three-river" ).show( "slow")
 });
